2669dd291b294fadcc56f59e767219e026b275ea
unknown <kochankap@gmail.com>
2021-10-27 21:28:24 +0200

    Get new element

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var text = "jajko czy kura";
 
+var dlugosc = text.length;
+
+var haslo1 = "";
+/* zamiana liter w kreski */
+
+for(var i = 0; i < dlugosc; i++)
+{
+    if(text[i] == " ") haslo1 = haslo1 + " ";
+    else haslo1 = haslo1 + "-";
+}
+
+function wypiszHaslo(){
+    var plansza = document.getElementById("text");
+    plansza.innerHTML = haslo1;
+}
+
+wypiszHaslo();
